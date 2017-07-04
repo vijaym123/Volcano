83cf9064f408da18bee42c9c31b92ba9e0787326
Vijay M <vijaym123@gmail.com>
2017-07-04 00:56:33 -0700

    Adding left and right

--- working-curses.py
+++ working-curses.py
@@ -6,16 +6,21 @@ import curses
 GPIO.setmode(GPIO.BOARD)
 
 
-MOTOR_PIN=32
-
+ESC_PIN=32
+SERVO_PIN=12
 f=500
 
-GPIO.setup(MOTOR_PIN,GPIO.OUT)
+GPIO.setup(ESC_PIN,GPIO.OUT)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
 
-MOTOR_PWM=GPIO.PWM(MOTOR_PIN,f)
 
-MOTOR_PWM.start(60)
-print "init 0"
+ESC_PWM=GPIO.PWM(ESC_PIN,f)
+SERVO_PWM = GPIO.PWM(SERVO_PIN, 100)
+
+
+ESC_PWM.start(60)
+SERVO_PWM.start(5)
+print "init"
 sleep(3)
 
 
@@ -28,23 +33,33 @@ stdscr.refresh()
 
 key = ''
 
-score = 60
-
+accelerate = 60
+turn = 15
 try:
     while key != ord('q'):
         stdscr.refresh()
-        print score
+        print accelerate, turn
         key = stdscr.getch()
         #stdscr.addch(20,25,key)
         #stdscr.refresh()
         if key == curses.KEY_UP:
-            score += 0.2
-            MOTOR_PWM.ChangeDutyCycle(score)
+            accelerate += 0.2
+            ESC_PWM.ChangeDutyCycle(accelerate)
         elif key == curses.KEY_DOWN:
-            score -= 0.2
-            MOTOR_PWM.ChangeDutyCycle(score)
-
+            accelerate -= 0.2
+            ESC_PWM.ChangeDutyCycle(accelerate)
+        elif key == curses.KEY_RIGHT:
+            turn -= 1
+            SERVO_PWM.ChangeDutyCycle(turn)
+            sleep(0.1)
+            SERVO_PWM.ChangeDutyCycle(0)               
+        elif key == curses.KEY_LEFT:
+            turn += 1
+            SERVO_PWM.ChangeDutyCycle(turn)
+            sleep(0.1)
+            SERVO_PWM.ChangeDutyCycle(0)  
 finally:
     curses.endwin()
-    MOTOR_PWM.stop()
+    ESC_PWM.stop()    
+    SERVO_PWM.stop()
     GPIO.cleanup()
